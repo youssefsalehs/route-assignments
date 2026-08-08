aacb6ecf95162888d695a20870406c516d0996a5
youssef <youssef.ashraf.saleh@gmail.com>
2026-08-08 13:44:03 +0300

    feat:  CRUD application with user management functionality - Added bonus.js with a function to find the longest common prefix in an array of strings.

---
+++ assignment3/bonus.js
@@ -0,0 +1,15 @@
+const longestCommonPrefix = function(strs) {
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (!strs[i].startsWith(prefix)) {
+      prefix = prefix.slice(0, -1);
+
+      if (prefix === "") {
+        return "";
+      }
+    }
+  }
+
+  return prefix;   
+};
